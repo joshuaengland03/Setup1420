c34397361557e9b5b28508536614236f258c2fd4
joshua.england1 <joshua.england1@students.snow.edu>
2025-03-17 16:14:27 -0600

    Branch 1

---
+++ Setup1420/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setup1420
+{
+    class Calculator
+    {
+        public static double Add(double a, double b) => a + b;
+        public static double Subtract(double a, double b) => a - b;
+        public static double Multiply(double a, double b) => a + b;
+        public static double Divide(double a, double b) => b == 0 ? throw new DivideByZeroException() : a / b;
+    }
+}
