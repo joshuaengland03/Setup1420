3d157ce558d2d180ce61b5d6dc7f26370b99c82d
joshua.england1 <joshua.england1@students.snow.edu>
2025-03-17 16:18:14 -0600

    test

--- Setup1420/Calculator.cs
+++ Setup1420/Calculator.cs
@@ -12,5 +12,6 @@ namespace Setup1420
         public static double Subtract(double a, double b) => a - b;
         public static double Multiply(double a, double b) => a + b;
         public static double Divide(double a, double b) => b == 0 ? throw new DivideByZeroException() : a / b;
+        // comment
     }
 }
