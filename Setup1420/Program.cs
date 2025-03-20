04d4feb565f3f8416e651affabbdc6a7704ad9c4
joshua.england1 <joshua.england1@students.snow.edu>
2025-03-20 15:11:49 -0600

    Zombie Stuff

--- Setup1420/Program.cs
+++ Setup1420/Program.cs
@@ -1,7 +1,54 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Runtime.InteropServices;
 
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/joshua.england1/source/repos/Setup1420/Setup1420/data.json";
+            string text = File.ReadAllText(path).ToLower();
+
+            var manual = JsonSerializer.Deserialize<ZombieSurvivalJournal>(text);
+
+            List<string> ignore = new List<string> { "the", "and", "is", "in", "of", "to", "a", "we", "it", "for", "on", "with" , "i", "an"};
+
+            Dictionary<string, int> keyWords = new Dictionary<string, int>();
+
+            foreach (var item in manual.Entries)
+            {
+                var words = item.Text.Split(" ");
+
+                foreach (var word in words)
+                {
+                    string result = word.Replace("!", "").Replace(".", "").Replace(",", "");
+                    if (ignore.Contains(word))
+                    {
+                        continue;
+                    }
+                    if (!keyWords.ContainsKey(result))
+                    {
+                        keyWords.Add(result, 1);
+
+                    }
+                    else
+                    {
+                        keyWords[result] += 1;
+                    }
+                }
+            }
+
+            foreach (var word in keyWords)
+            {
+                Console.WriteLine($"{word.Key} {word.Value}");
+            }
+        }
     }
 }
---
+++ Setup1420/Zombies.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace program
+{
+    [Serializable]
+    class ZombieSurvivalJournal
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("entries")]
+        public List<JournalEntry> Entries { get; set; }
+
+
+    }
+
+
+    public class JournalEntry
+    {
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
