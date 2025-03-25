f31987bc7374abf22513a8a34ce979175d930336
joshua.england1 <joshua.england1@students.snow.edu>
2025-03-25 15:43:19 -0600

    test demo

--- Setup1420/SpyToolkit.cs
+++ Setup1420/SpyToolkit.cs
@@ -1,70 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DemoSolution;
 
-namespace SpyTools
+public class SpyToolkit
 {
-    class SpyToolkit
+    private readonly List<SpyGadget> gadgets = new List<SpyGadget>();
+
+    public SpyToolkit() { }
+
+    public void AddGadget(string name, string category, int powerLevel = 50, bool isActive = true)
     {
-        List<SpyGadget> SpyGadgets { get; set; } = new List<SpyGadget>();
-        public SpyToolkit()
+        if (gadgets.Any(g => g.Name == name))
         {
-
+            Console.WriteLine($"{name} already exists.");
+            return;
         }
+        gadgets.Add(new SpyGadget { Name = name, Category = category, PowerLevel = powerLevel, IsActive = isActive });
+        Console.WriteLine($"{name} added to toolkit.");
+    }
+
+    public List<SpyGadget> GetActiveGadgets(string category = null)
+    {
+        return new List<SpyGadget>();
+
+    }
+
+    public void DeactivateGadget(string name)
+    {
+
+    }
+
+    public static bool PowerCheck(List<SpyGadget> gadgets, int minPower)
+    {
+        return false;
+    }
 
 
-        public void AddGadget(string name, string category, int powerLevel = 50, bool isActive = true)
-        {
-            SpyGadget spyGadget = new SpyGadget();
-            spyGadget.Name = name;
-            spyGadget.Category = category;
-            spyGadget.PowerLevel = powerLevel;
-            spyGadget.IsActive = isActive;
-
-            bool containsGadget = SpyGadgets.Any(s => s.Name == name);
-
-            if (containsGadget)
-            {
-                Console.WriteLine($"{name} already exists.");
-                return;
-            }
-
-            SpyGadgets.Add(spyGadget);
-            Console.WriteLine($"{name} added to toolkit.");
-        }
-
-
-        public List<SpyGadget> GetActiveGadgets(string category = null)
-        {
-            return SpyGadgets.Where(s => s.IsActive && (category == null || s.Category == category)).ToList();
-        }
-
-        public void DeactivateGadget(string name)
-        {
-            SpyGadget gadget = new SpyGadget();
-            gadget = null;
-
-            foreach (var gadgets in SpyGadgets)
-            {
-                if (gadgets.Name == name)
-                {
-                    gadget = gadgets;
-                    break;
-                }
-            }
-
-            if (gadget != null)
-            {
-                Console.WriteLine($"{name} deactivated.");
-                gadget.IsActive = false;
-            }
-            else
-            {
-                Console.WriteLine($"{name} not found.");
-            }
-        }
+    public void DebugMission(string missionName, int requiredPower)
+    {
 
     }
 }
